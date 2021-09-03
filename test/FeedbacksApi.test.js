8fac9e0ecb5b55f690eb57b247da4c8101d658ae
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-09-03 20:56:25 +0700

    Preparing for API endpoint tests.

---
+++ test/FeedbacksApi.test.js
@@ -0,0 +1,11 @@
+describe("API Route: /api/feedbacks", () => {
+  const API_ENDPOINT = "http://localhost:3000/api/feedbacks";
+
+  describe("Method: GET", () => {
+    it("Should be a JSON");
+    it("Should have a `message` and `data` property");
+    it("The `message` property should be a string");
+    it("The `data` property should be an array");
+    it("Should be and always returning 200 status");
+  });
+});
